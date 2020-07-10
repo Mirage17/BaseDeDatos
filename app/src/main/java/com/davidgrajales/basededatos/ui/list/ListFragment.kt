8a9a1070fa8cca1e23dafeaa32f85474ba908c4d
David Grajales <david.grajalesc@udea.edu.co>
2020-07-10 18:25:21 -0500

    room eyecandy improvements

--- app/src/main/java/com/davidgrajales/basededatos/ui/list/ListFragment.kt
+++ app/src/main/java/com/davidgrajales/basededatos/ui/list/ListFragment.kt
@@ -46,7 +46,7 @@ class ListFragment : Fragment() {
         deudoresRVAdapter.notifyDataSetChanged()
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return root
     }
 
  
